d4f70757a31578231f6727ae4e1984d730d074b9
martin-martin <breuss.martin@gmail.com>
2018-08-11 18:26:20 +0200

    add and connect static style file

--- homepage/homepage/settings.py
+++ homepage/homepage/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# if things don't work, JUST DO THIS!
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "portfolio/static"),
+    '/var/www/static/',
+]
