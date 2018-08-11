93c726a9f36b58e71e8ad468983482094293c04a
martin-martin <breuss.martin@gmail.com>
2018-08-11 15:41:36 +0200

    route to portfolio template

--- homepage/portfolio/views.py
+++ homepage/portfolio/views.py
@@ -1,5 +1,8 @@
+import random
 from django.shortcuts import render
 
 # Create your views here.
 def main(request):
-    return render(request, 'base.html', {'welcome': "my portfolio goes here!"})
+    msg_list = ["i really like buidling things", "hei stranger!"]
+    msg = random.choice(msg_list)
+    return render(request, 'portfolio.html', {'msg': msg})
