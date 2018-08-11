3aa33a5ac501c6641c74f94156a57dc0650ddfb3
martin-martin <breuss.martin@gmail.com>
2018-08-11 15:22:27 +0200

    fix: field name and dependencies

--- homepage/portfolio/models.py
+++ homepage/portfolio/models.py
@@ -13,15 +13,15 @@ class ProjectType(models.Model):
 class Project(models.Model):
     project_type = models.ForeignKey(ProjectType,
                                         # allow having no ProjectType association
-                                        on_delete=SET_NULL,
+                                        models.SET_NULL,
                                         blank=True, null=True)
     title = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='/folder/url',
+    image = models.ImageField(upload_to='folder/url',
                                 blank=True, null=True)
     description = models.CharField(max_length=1000)  # maybe TextField()
-    live_link = models.UrlField(max_length=2000)
-    source_code = models.UrlField(max_length=2000)
-    blog_link = models.UrlField(max_length=2000)
+    live_link = models.URLField(max_length=2000)
+    source_code = models.URLField(max_length=2000)
+    blog_link = models.URLField(max_length=2000)
 
     def __str__(self):
         return self.title
