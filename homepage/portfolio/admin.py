d5debfe22fe2a1113edd8a43b09ecebe5198ae58
martin-martin <breuss.martin@gmail.com>
2018-08-11 15:50:35 +0200

    register portfolio models

--- homepage/portfolio/admin.py
+++ homepage/portfolio/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ProjectType, Project, Technology
 
 # Register your models here.
+admin.site.register(ProjectType)
+admin.site.register(Project)
+admin.site.register(Technology)
