80f66ab9a3ce245ea52208acf0473f183f7297c7
martin-martin <breuss.martin@gmail.com>
2018-08-11 16:52:45 +0200

    port portfolio layout

--- homepage/portfolio/views.py
+++ homepage/portfolio/views.py
@@ -1,8 +1,12 @@
 import random
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
 def main(request):
     msg_list = ["i really like buidling things", "hei stranger!"]
     msg = random.choice(msg_list)
-    return render(request, 'portfolio.html', {'msg': msg})
+    projects = Project.objects.all()
+    return render(request, 'portfolio.html',
+                    {'msg': msg,
+                        'projects': projects})
