a8d48174a1434a2fc7ad9e23f1c8311d74dad086
martin-martin <breuss.martin@gmail.com>
2018-08-11 15:35:09 +0200

    create and register base template

--- homepage/homepage/settings.py
+++ homepage/homepage/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'homepage.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'portfolio/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- homepage/portfolio/views.py
+++ homepage/portfolio/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def main(request):
-    return HttpResponse("my potfolio goes here!")
+    return render(request, 'base.html', {'welcome': "my portfolio goes here!"})
